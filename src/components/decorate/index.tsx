085964e76b246fbc2933e1ae2e928e07b2196d30
zhangcan <zhangcan@xiaoshouyi.com>
2019-02-11 19:23:11 +0800

    基本完成装饰视图

--- src/components/decorate/index.tsx
+++ src/components/decorate/index.tsx
@@ -8,35 +8,99 @@
 
 import React, { Children } from 'react'
 import { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Block from "../block";
+
+
+enum MatrixState {
+  Hidden,
+  Display
+}
+
+let H = MatrixState.Hidden
+let D = MatrixState.Display
 
 export default class Decorate extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.leftDecorate}></View>
+        <View style={styles.leftDecorate}>
+          {this.renderShapeBlock(
+            [[H, D, D, H],
+             [D, D, H, H]])}
+          {this.renderShapeBlock(
+            [[D, D, D, H],
+             [H, D, H, H]])}
+          {this.renderShapeBlock(
+            [[D, D, H, H],
+             [D, D, H, H]])}
+          {this.renderShapeBlock(
+            [[H, D, H, H],
+             [D, D, D, H]])}
+          {this.renderShapeBlock(
+            [[D, H, H, H],
+             [D, D, D, H]])}
+          {this.renderShapeBlock(
+            [[D, D, D, D],
+             [H, H, H, H]])}      
+        </View>
         <View style={styles.centerDecorate}>
           <View style={styles.topBorder}>
-            <View style={[styles.block, styles.longBlock]}/>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
+            <View style={[styles.block, styles.longBlock]} />
+            <View style={styles.block} />
+            <View style={styles.block} />
+            <View style={styles.block} />
+            <View style={styles.block} />
             <Text style={styles.title}>俄罗斯方块</Text>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
-            <View style={styles.block}/>
-            <View style={[styles.block, styles.longBlock]}/>
+            <View style={styles.block} />
+            <View style={styles.block} />
+            <View style={styles.block} />
+            <View style={styles.block} />
+            <View style={[styles.block, styles.longBlock]} />
           </View>
           <View style={styles.children}>
             {this.props.children}
-          </View>          
+          </View>
         </View>
-        <View style={styles.rightDecorate}></View>
+        <View style={styles.rightDecorate}>
+        {this.renderShapeBlock(
+            [[D, D, H, H],
+             [H, D, D, H]])}
+          {this.renderShapeBlock(
+            [[H, D, H, H],
+             [D, D, D, H]])}
+          {this.renderShapeBlock(
+            [[D, D, H, H],
+             [D, D, H, H]])}
+          {this.renderShapeBlock(
+            [[D, D, D, H],
+             [H, D, H, H]])}
+          {this.renderShapeBlock(
+            [[D, D, D, H],
+             [D, H, H, H]])}
+          {this.renderShapeBlock(
+            [[H, H, H, H],
+             [D, D, D, D]])}   
+        </View>
       </View>
     );
   }
+
+  //2行4列的2维数组来表示俄罗斯方块的形状
+  renderShapeBlock = (matrix: MatrixState[][]) => {
+    return (
+      <View style={styles.shapeBlock}>
+        {matrix.map((l: MatrixState[]) => {
+          return l.map(v => {
+            return <Block color={v == D ? 'black' : 'rgba(0,0,0,0)'} />
+          })
+        })}
+      </View>
+    );
+  }
+
+
+
 }
 
 const styles = StyleSheet.create({
@@ -59,14 +123,14 @@ const styles = StyleSheet.create({
   topBorder: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    height: 20,    
+    height: 20,
   },
   title: {
-    fontSize: 15,        
+    fontSize: 15,
   },
   block: {
-    width:10,
-    height:10,
+    width: 10,
+    height: 10,
     backgroundColor: 'black',
   },
 
@@ -75,14 +139,28 @@ const styles = StyleSheet.create({
   },
 
   leftDecorate: {
-    width: 40,
-    backgroundColor: 'red'
+    width: 44,    
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
   rightDecorate: {
-    width: 40,
-    backgroundColor: 'green'
+    width: 44,    
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
   children: {
     flex: 1
+  },
+  shapeBlock: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    flexDirection: 'column',
+    width: 20,
+    height: 56,
+    marginBottom: 10,
   }
 });
